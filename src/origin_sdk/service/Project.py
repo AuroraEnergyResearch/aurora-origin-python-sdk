ee18666fdaf4477ecf060e953bddb2057824c0c4
Chris Johnstone <5574633+frisbee09@users.noreply.github.com>
2023-05-12 12:22:15 +0100

    ORIGIN-361 Scenario tests

--- src/origin_sdk/service/Project.py
+++ src/origin_sdk/service/Project.py
@@ -1,9 +1,6 @@
 import logging
 from origin_sdk.OriginSession import OriginSession
 from typing import Optional
-from tempfile import TemporaryDirectory
-import pandas as pd
-from io import StringIO
 
 logger = logging.getLogger(__name__)
 
@@ -20,6 +17,9 @@ class Project:
         """
         return self.project.get(key)
 
+    def refresh(self):
+        self.project = self.session.get_project(self.project_id)
+
     def pin(self):
         if not self.project.get("isProjectPinned"):
             self.session.pin_project(self.project_id)
@@ -27,3 +27,72 @@ class Project:
     def unpin(self):
         if self.project.get("isProjectPinned"):
             self.session.unpin_project(self.project_id)
+            
+    def get_or_create_scenario_by_name(
+        self,
+        scenario_name: str,
+        base_scenario_id: Optional[str] = None
+    ):
+        """Either gets a scenario from this project or creates a scenario in
+        this project with the name specified. In the case of create, requires a
+        base_id.
+        
+        Arguments:
+            scenario_name: str - The name of the scenario to look for
+            base_id: Optional, str - The ID of the scenario to use as a base
+        """
+        self.refresh()
+        
+        scenarios = [
+            scenario
+            for scenario in self.project.get("scenarios")
+            if scenario.get("name").lower() == scenario_name.lower()
+        ]
+    
+        scenario = None
+        if len(scenarios) > 0:
+            # We found a scenario with a name match!
+            # Select it to return
+            scenario = scenarios[0]
+        elif base_scenario_id is not None:
+            # No scenario found, create it.
+            scenario = self.session.create_scenario(
+                {
+                    "projectGlobalId": self.project.get("projectGlobalId"),
+                    "baseScenarioGlobalId": base_scenario_id,
+                    "name": scenario_name,
+                }
+            )
+        else:
+            logger.warn((
+                "No scenario found in project ",
+                f"{self.project.get('name')}",
+                f"' with name '{scenario_name}'"
+            ))
+            
+        return scenario
+    
+    @staticmethod
+    def get_or_create_project_by_name(session: OriginSession, name: str, pin_project: bool = False):
+        """Either creates a project with this name, or returns the project that
+        already exists with the same name. In the event there is more than one
+        project, returns the first one returned by the service.
+    
+        Arguments:
+            session: OriginSession - The session to make API calls with
+            name: str - The name of the project to find/create
+            pin_project: Optional, bool - Whether to pin the project if it's not
+            already, defaults to False
+        """
+    
+        all_projects = [p for p in session.get_projects() if p.get("name") == name]
+    
+        if len(all_projects) > 0:
+            project = all_projects[0]
+        else:
+            project = session.create_project({"name": name})
+    
+        if project.get("isProjectPinned") is False and pin_project is True:
+            session.pin_project(project.get("projectGlobalId"))
+    
+        return Project(session=session, project_id=project.get("projectGlobalId"))
