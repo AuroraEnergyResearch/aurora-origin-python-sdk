b070d4dd47febc5baecccf384bca8e7579e9b90b
Jebbin <jebbin.joseph@auroraer.com>
2023-10-17 15:04:25 +0100

    Update Monthly Values

--- src/origin_sdk/gql/queries/input_queries/technology.py
+++ src/origin_sdk/gql/queries/input_queries/technology.py
@@ -149,7 +149,14 @@ def get_technology_gql(tech_config: Any):
 
 
 def update_endo_technology_gql(tech_config: Any):
-    _, endo_params, _, endo_defs = get_endo_exo_param_list_from_config(tech_config)
+    (
+        _,
+        _,
+        endo_params_yearly,
+        endo_params_monthly,
+        _,
+        endo_defs,
+    ) = get_endo_exo_param_list_from_config(tech_config)
     return f"""mutation (
     $sessionId: String!,
     $parameter: EndogenousPlantVariable!,
@@ -168,8 +175,9 @@ def update_endo_technology_gql(tech_config: Any):
             subTechnology: $subTechnology) {
                 tree_to_string(
                     get_endo_param_and_def_tree(
-                        endo_params,
-                        endo_defs
+                        endo_params_yearly=endo_params_yearly,
+                        endo_params_monthly=endo_params_monthly,
+                        endo_defs=endo_defs
                     ),
                     level=4
                 )
@@ -179,7 +187,14 @@ def update_endo_technology_gql(tech_config: Any):
 
 
 def update_exo_technology_gql(tech_config: Any):
-    exo_params, _, _, _ = get_endo_exo_param_list_from_config(tech_config)
+    (
+        exo_params_yearly,
+        exo_params_monthly,
+        _,
+        _,
+        _,
+        _,
+    ) = get_endo_exo_param_list_from_config(tech_config)
     return f"""mutation (
     $sessionId: String!,
     $parameter: ExogenousPlantVariable!,
@@ -200,7 +215,8 @@ def update_exo_technology_gql(tech_config: Any):
             subTechnology: $subTechnology) {
                 tree_to_string(
                     get_exo_param_and_def_tree(
-                        exo_params
+                        exo_params_yearly=exo_params_yearly,
+                        exo_params_monthly=exo_params_monthly
                     )
                 )
             }
