66cb06c6ac03c2ba0840b73bff22548d308fcf11
Joe Dyer <joe.dyer@auroraer.com>
2024-11-12 14:13:57 +0000

    apollo-973 Use a env var code build actually sets and show all found env vars

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,11 @@ import os
 pytest_plugins = ["input_tests.utils_for_testing"]
 
 
+for key, value in os.environ.items():
+    print(f"{key}: {value}")
+
 # Disable tests in code build due to lack of internet access
-if os.getenv("CODEBUILD_CONTAINER_NAME"):
+collect_ignore = ["setup.py"]
+if os.getenv("PYTEST_IGNORE"):
+    collect_ignore.append("tests/*")
     collect_ignore_glob = ["*.py"]
