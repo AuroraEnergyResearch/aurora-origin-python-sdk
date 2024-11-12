cfe05caab4a7ba08fe13998eff481da174626f73
Joe Dyer <joe.dyer@auroraer.com>
2024-11-12 15:20:56 +0000

    apollo-973 Ensure only placeholder is run

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,15 @@
 import os
 
-pytest_plugins = ["input_tests.utils_for_testing"]
+import pytest
 
+pytest_plugins = ["input_tests.utils_for_testing"]
 
 for key, value in os.environ.items():
     print(f"{key}: {value}")
 
 # Disable tests in code build due to lack of internet access
 if os.getenv("CODEBUILD_BUILD_ID"):
-    collect_ignore_glob = ["*test_*.py"]
+
+    @pytest.hookimpl(tryfirst=True)
+    def pytest_collection_modifyitems(config, items):
+        items[:] = [item for item in items if "test_placeholder" in item.nodeid]
